ee3b5d215331422701be2ee43ad9f556589ba9e8
satya-666 <i.m.satya.666@gmail.com>
2025-01-19 22:15:44 +0530

    Time: 12 ms (81.94%), Space: 12.5 MB (7.94%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,9 +1,16 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        kim = ""
+        max_len = max(len(word1), len(word2))
+        word1 = word1.ljust(max_len)
+        word2 = word2.ljust(max_len)
 
-        for i, j in zip(word1.ljust(len(word2)), word2.ljust(len(word1))):
-            kim += i
-            kim += j
+        kim = ''.join(i + j for i, j in zip(word1, word2))
 
         return(kim.replace(" ", ""))
+        # kim = ""
+
+        # for i, j in zip(word1.ljust(len(word2)), word2.ljust(len(word1))):
+        #     kim += i
+        #     kim += j
+
+        # return(kim.replace(" ", ""))
