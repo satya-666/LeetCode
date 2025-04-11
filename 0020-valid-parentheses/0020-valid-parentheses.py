901fd34d99259a9c7513e210e5d2c552a11019fc
satya-666 <i.m.satya.666@gmail.com>
2025-04-11 22:34:33 +0530

    Time: 4 ms (26.99%), Space: 12.5 MB (84.83%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map:
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False
+                stack.pop()
+        
+        return not stack
