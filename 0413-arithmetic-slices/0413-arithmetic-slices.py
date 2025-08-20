999b3569f88ca032b25b69d07bf87b18b066cebb
satya-666 <i.m.satya.666@gmail.com>
2025-08-20 08:14:15 +0530

    Time: 0 ms (100%), Space: 12.7 MB (67.75%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -1,20 +1,26 @@
 class Solution(object):
     def numberOfArithmeticSlices(self, nums):
-        if len(nums) < 3:
-            return 0
-        streak = 0
-        count = 0
-        diff = nums[1] - nums[0]
-        for i in range(1,len(nums)):
-            curr_diff = nums[i] - nums[i-1]
-            if curr_diff == diff:
-                streak += 1
-            if curr_diff != diff :
-                diff = curr_diff
-                streak = 1
-            if streak == 2:
-                count += 1
-            if streak > 2:
-                count += streak-1
+        n=len(nums)
+        lst=[0]*n
+        for i in range(2,n):
+            if nums[i]-nums[i-1] == nums[i-1]-nums[i-2]:
+                lst[i]=1+lst[i-1]
+        return(sum(lst))
+        # if len(nums) < 3:
+        #     return 0
+        # streak = 0
+        # count = 0
+        # diff = nums[1] - nums[0]
+        # for i in range(1,len(nums)):
+        #     curr_diff = nums[i] - nums[i-1]
+        #     if curr_diff == diff:
+        #         streak += 1
+        #     if curr_diff != diff :
+        #         diff = curr_diff
+        #         streak = 1
+        #     if streak == 2:
+        #         count += 1
+        #     if streak > 2:
+        #         count += streak-1
 
-        return(count)
+        # return(count)
