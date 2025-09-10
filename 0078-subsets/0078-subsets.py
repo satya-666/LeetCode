01fe1643e115d6c2b9d0c18b24ee2f64f6d4df6a
satya-666 <i.m.satya.666@gmail.com>
2025-09-10 22:35:16 +0530

    Time: 3 ms (15.15%), Space: 12.8 MB (20.84%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,27 @@
 class Solution(object):
     def subsets(self, nums):
-        subsequences = []
         n = len(nums)
-        for i in range(0, 1 << n):
-            subseq = []
-            for j in range(n):
-                if i & (1 << j):
-                    subseq.append(nums[j])
-            subsequences.append(subseq)
-        subsequences.sort(key=lambda x: (len(x), x))
-        return(subsequences)
+        nums.sort()
+        res = []
+        def helper(ind,ds):
+            if ind <= n:
+                res.append(ds[:])
+            for i in range(ind,n):
+                # if nums[i] == nums[i-1]:
+                #     continue
+                ds.append(nums[i])
+                helper(i+1,ds)
+                ds.pop()
+        helper(0,[])
+        return res
+        # subsequences = []
+        # n = len(nums)
+        # for i in range(0, 1 << n):
+        #     subseq = []
+        #     for j in range(n):
+        #         if i & (1 << j):
+        #             subseq.append(nums[j])
+        #     subsequences.append(subseq)
+        # subsequences.sort(key=lambda x: (len(x), x))
+        # return(subsequences)
                 
