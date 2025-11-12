6982f5b28e92cdfc46618d22e95e67ce7b138fd1
satya-666 <i.m.satya.666@gmail.com>
2025-11-12 23:10:28 +0530

    Time: 48 ms (5.62%), Space: 43.7 MB (5.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,21 @@
 class Solution(object):
     def maxProfit(self, prices):
-        count = 0
-
-        for i in range(len(prices)-1):
-            if prices[i] < prices[i+1]:
-                count += prices[i+1] - prices[i]
-        return(count)
+        dp = [[-1]*2 for _ in range(len(prices))]
+        def helper(ind,canBuy):
+            profit = 0
+            if ind >= len(prices):
+                return 0
+            if dp[ind][canBuy] != -1:
+                return dp[ind][canBuy]
+            if canBuy == 1:
+                buy = -prices[ind] + helper(ind+1,0)
+                notBuy = 0 + helper(ind+1,1)
+                profit += max(buy,notBuy)
+                dp[ind][canBuy] = profit
+            else:
+                sell = prices[ind] + helper(ind+1,1)
+                notSell = 0 + helper(ind+1,0)
+                profit += max(sell,notSell)
+                dp[ind][canBuy] = profit
+            return profit
+        return helper(0,1)
