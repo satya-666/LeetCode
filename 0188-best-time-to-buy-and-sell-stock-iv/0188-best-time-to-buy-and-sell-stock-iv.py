d1f7743d628249eb5e3785fb62877ab6da2be6b0
satya-666 <i.m.satya.666@gmail.com>
2025-11-14 22:48:25 +0530

    Time: 473 ms (7.24%), Space: 58.8 MB (5.58%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxProfit(self, k, prices):
+        dp = {}
+        def helper(ind,canBuy,count):
+            profit = 0
+            if ind >= len(prices) or count >= k:
+                return 0
+            if (ind,canBuy,count) in dp:
+                return dp[(ind,canBuy,count)]
+            if canBuy == 1:
+                buy = -prices[ind] + helper(ind+1,0,count)
+                notBuy = 0 + helper(ind+1,1,count)
+                profit = max(buy,notBuy)
+                dp[(ind,canBuy,count)] = profit
+            else:
+                sell = prices[ind] + helper(ind+1,1,count+1)
+                notSell = 0 + helper(ind+1,0,count)
+                profit = max(sell,notSell)
+                dp[(ind,canBuy,count)] = profit
+            return profit
+        return helper(0,1,0)
