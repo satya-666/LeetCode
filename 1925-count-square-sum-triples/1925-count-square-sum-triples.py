b62058b911e6d231edb6d7d80b914e5614989076
satya-666 <i.m.satya.666@gmail.com>
2025-12-09 03:07:09 +0530

    Time: 363 ms (42.42%), Space: 12.3 MB (81.82%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countTriples(self, n):
+        count = 0
+        for a in range(1, n+1):
+            for b in range(1, n+1):
+                c_square = a*a + b*b
+                c = int(c_square**0.5)
+                if c*c == c_square and c <= n:
+                    count += 1
+        return count
+
