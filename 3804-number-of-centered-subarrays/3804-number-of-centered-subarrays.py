6976d3cf7ab293022c7f5560b191fa18e45fe016
satya-666 <i.m.satya.666@gmail.com>
2026-01-20 19:48:16 +0530

    Time: 447 ms (35.27%), Space: 12.7 MB (21.05%) - LeetHub

--- 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.py
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.py
@@ -1,11 +1,27 @@
 class Solution(object):
     def centeredSubarrays(self, nums):
         n = len(nums)
-        count=0
+        res = 0
         for i in range(n):
-            for j in range(i,n+1):
-                sub = nums[i:j]
-                if sum(sub) in sub:
-                    count += 1
-        return count
+            s = 0
+            freq = {}
+            for j in range(i, n):
+                s += nums[j]
+                if nums[j] in freq:
+                    freq[nums[j]] += 1
+                else:
+                    freq[nums[j]] = 1
+                if s in freq and freq[s] > 0:
+                    res += 1
+        return res
+
+        # n = len(nums)
+        # count=0
+        
+        # for i in range(n):
+        #     for j in range(i,n+1):
+        #         sub = nums[i:j]
+        #         if sum(sub) in sub:
+        #             count += 1
+        # return count
         
