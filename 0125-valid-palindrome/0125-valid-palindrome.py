35608c05cbb0b8901fae35e47bb13aed7398021b
satya-666 <i.m.satya.666@gmail.com>
2026-02-13 21:25:12 +0530

    Time: 159 ms (11.4%), Space: 52.2 MB (5.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,29 @@
 class Solution(object):
     def isPalindrome(self, s):
-        k = []
-        for i in s:
-            if i.isalnum(): 
-                k.append(i.lower())
+        ns = ''
+        for ch in s:
+            if ch.isalnum(): 
+                ns+=(ch.lower())
+        n = len(ns)
+        def pd(i):
+            if i >= n//2:
+                return True
+            if ns[i] != ns[n-i-1]:
+                return False
+            return pd(i+1)
+        return pd(0)
+        # k = []
+        # for i in s:
+        #     if i.isalnum(): 
+        #         k.append(i.lower())
 
-        start, end = 0, len(k) - 1
-        while start < end:
-            if k[start] != k[end]:
-                return(2==3)
-                break
-            start += 1
-            end -= 1
-        else:
-            return(2==2)
+        # start, end = 0, len(k) - 1
+        # while start < end:
+        #     if k[start] != k[end]:
+        #         return(2==3)
+        #         break
+        #     start += 1
+        #     end -= 1
+        # else:
+        #     return(2==2)
         
